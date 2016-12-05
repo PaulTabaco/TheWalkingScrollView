d81cacbb17dc158ec8528e9b0c2bb04cca73bfb4
Paul <comrade_pasha@ukr.net>
2016-12-06 00:36:36 +0200

    v001 Swift 2.3

--- TheWalkingScrollView/ViewController.swift
+++ TheWalkingScrollView/ViewController.swift
@@ -9,16 +9,27 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var scrollView: UIScrollView!
 
+    let WIDTH: CGFloat = 240.0
+    let HEIGHT: CGFloat = 397.0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        for i in 0..<5 {
+            let img = UIImage(named: "\(i)")
+            let imgView = UIImageView(image: img)
+            scrollView.addSubview(imgView)
+            
+            imgView.frame = CGRectMake(-WIDTH + (WIDTH * CGFloat(i)), 270, WIDTH, HEIGHT)
+        }
+        
+        scrollView.contentSize = CGSizeMake(WIDTH * 5, scrollView.frame.size.height)
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+    
 
 
 }
